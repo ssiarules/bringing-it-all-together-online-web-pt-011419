6e88ccda01adbaa390a469bf0eba7fc39248bbbb
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-09 15:00:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,7 @@ end
 def self.find_by_id(id)
    sql = "SELECT * FROM dogs WHERE id = ? LIMIT 1"
     result = DB[:conn].execute(sql, id)[1]
-   Dog.new(id:[1],name:[2], breed:[3])
+   Dog.new(id:[1],name:[2], breed:[3]).flatten
  end
 
 
