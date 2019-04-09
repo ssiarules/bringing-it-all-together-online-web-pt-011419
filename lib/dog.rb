261709636e5791fbbe8e8ee385e4ca921451765b
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-09 15:31:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,7 @@ def self.find_by_id(id)
  end
 
  def self.find_or_create_by(name:, breed:)
-    dog = self.new
+    dog = self.new(name:, breed: )
      dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
      if !dog.empty?
        dog_data = dog[0]
